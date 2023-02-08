d053afc0c4e4818b799ee8b5113a264bfc039744
Josiah Green <JosiahGreen90@gmail.com>
2023-02-08 14:55:08 -0800

    Initial Commit

---
+++ iFind/iFindApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFindApp.swift
+//  iFind
+//
+//  Created by Josiah Green on 2/8/23.
+//
+
+import SwiftUI
+
+@main
+struct iFindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
