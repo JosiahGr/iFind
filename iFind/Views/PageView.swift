d3061ff59769df69519befaee63b074b1914599d
Josiah Green <JosiahGreen90@gmail.com>
2023-04-11 16:48:52 -0700

    WIP

--- iFind/Views/PageView.swift
+++ iFind/Views/PageView.swift
@@ -15,78 +15,73 @@ struct PageView: View {
     @State private var fourthThumbnail = false
     @State private var fifthThumbnail = false
     
-    @State private var firstIsSelected = 0
-    @State private var secondIsSelected = 0
-    @State private var thirdIsSelected = 0
-    @State private var fourthIsSelected = 0
-    @State private var fifthIsSelected = 0
+    @State private var firstIsSelected = false
+    @State private var secondIsSelected = false
+    @State private var thirdIsSelected = false
+    @State private var fourthIsSelected = false
+    @State private var fifthIsSelected = false
     
     @State private var thumbnailImages = [0: "thumbnail1", 1: "thumbnail2", 2: "thumbnail1", 3: "thumbnail2", 4: "thumbnail1"]
     @State private var imageIndex = 0
     
-    @State var counter = 0
+    func removeFromDict(index: Int) {
+        
+        thumbnailImages.removeValue(forKey: index)
+    }
     
     var body: some View {
         
         let imageValue = thumbnailImages[imageIndex]
         
         ZStack {
-            
             Image("beeBackground-01")
                 .scaledToFill()
                 .edgesIgnoringSafeArea(.all)
             
-//            ThumbnailView(thumbnailImages: images, color: .orange)
-//                .padding(.bottom, 60)
-//                .padding(.trailing, 20)
-            
-
-            
-            if firstIsSelected == 1 {
-//                imageIndex.remove(at: 4)
-                Image("bee-Green-01")
-                    .scaledToFill()
-                    .edgesIgnoringSafeArea(.all)
+            Group {
+                if firstIsSelected == true {
+                    Image("bee-Green-01")
+                        .scaledToFill()
+                        .edgesIgnoringSafeArea(.all)
+                    
+                    // remove from thumbnail array
+                    // set highlight to opacity 1.0
+                    // go to next object in array
+                }
                 
-                // remove from thumbnail array
-                // set highlight to opacity 1.0
-                // go to next object in array
-            }
-            
-            if secondIsSelected == 1 {
+                if secondIsSelected == true {
+                    Image("bee-Yellow-01")
+                        .scaledToFill()
+                        .edgesIgnoringSafeArea(.all)
+                }
                 
-                Image("bee-Yellow-01")
-                    .scaledToFill()
-                    .edgesIgnoringSafeArea(.all)
-            }
-            
-            if thirdIsSelected == 1 {
+                if thirdIsSelected == true {
+                    Image("bee-lightGreen-01")
+                        .scaledToFill()
+                        .edgesIgnoringSafeArea(.all)
+                }
                 
-                Image("bee-lightGreen-01")
-                    .scaledToFill()
-                    .edgesIgnoringSafeArea(.all)
-            }
-            
-            if fourthIsSelected == 1 {
-                Image("bee-Red-01")
-                    .scaledToFill()
-                    .edgesIgnoringSafeArea(.all)
-            }
-            
-            if fifthIsSelected == 1 {
-                Image("bee-Blue-01")
-                    .scaledToFill()
-                    .edgesIgnoringSafeArea(.all)
+                if fourthIsSelected == true {
+                    Image("bee-Red-01")
+                        .scaledToFill()
+                        .edgesIgnoringSafeArea(.all)
+                }
+                
+                if fifthIsSelected == true {
+                    Image("bee-Blue-01")
+                        .scaledToFill()
+                        .edgesIgnoringSafeArea(.all)
+                }
             }
             
             Group {
-                
                 if firstThumbnail == true {
                     Button {
-                        firstIsSelected = 1
-                        counter += 1
+                        firstIsSelected = true
                         firstThumbnail = false
                         secondThumbnail = true
+                        imageIndex = 1
+                        removeFromDict(index: 0)
                         
                     } label: {
                         Text(" ")
@@ -99,10 +94,11 @@ struct PageView: View {
                 
                 if secondThumbnail == true {
                     Button {
-                        secondIsSelected = 1
-                        counter += 1
+                        secondIsSelected = true
                         secondThumbnail = false
                         thirdThumbnail = true
+                        imageIndex = 2
+                        removeFromDict(index: 1)
                         
                     } label: {
                         Text(" ")
@@ -115,10 +111,11 @@ struct PageView: View {
                 
                 if thirdThumbnail == true {
                     Button {
-                        thirdIsSelected = 1
-                        counter += 1
+                        thirdIsSelected = true
                         thirdThumbnail = false
                         fourthThumbnail = true
+                        imageIndex = 3
+                        removeFromDict(index: 2)
                         
                     } label: {
                         Text(" ")
@@ -131,10 +128,11 @@ struct PageView: View {
                 
                 if fourthThumbnail == true {
                     Button {
-                        fourthIsSelected = 1
-                        counter += 1
+                        fourthIsSelected = true
                         fourthThumbnail = false
                         fifthThumbnail = true
+                        imageIndex = 4
+                        removeFromDict(index: 3)
                         
                     } label: {
                         Text(" ")
@@ -147,10 +145,10 @@ struct PageView: View {
                 
                 if fifthThumbnail == true {
                     Button {
-                        fifthIsSelected = 1
-                        counter += 1
+                        fifthIsSelected = true
                         fifthThumbnail = true
-                        
+                        imageIndex = 0
+                        removeFromDict(index: 4)
                         
                     } label: {
                         Text(" ")
@@ -162,39 +160,57 @@ struct PageView: View {
                 }
             }
             
-            HStack {
-                Spacer()
-                
-                VStack {
-                    Spacer()
-                    
-                    Button(action: {
-                        imageIndex += 1
-                        
-                        if imageIndex > thumbnailImages.count-1 {
-                            imageIndex = 0
-                        }
-                        
-                    }, label: {
-                        Image("\(imageValue!)")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 100, height: 100)
-                    })
-                    .background(.orange)
-                    .cornerRadius(100.0)
-                }.padding(.bottom, 80)
-            }.padding(.trailing, 40)
             
-            
-            if counter == 5 {
+            if thumbnailImages.count == 0 {
                 Text("Great Work!")
                     .font(.title)
                     .foregroundColor(.white)
                     .padding(50)
                     .background(.orange)
+            } else {
+                HStack {
+                    Spacer()
+                    
+                    VStack {
+                        Spacer()
+                        
+                        Button(action: {
+                            
+                            if imageIndex < thumbnailImages.count - 1 {
+                                imageIndex += 1
+                                
+                            } else if imageIndex > thumbnailImages.count - 1 {
+                                imageIndex = 0
+                            }
+                            
+                            switch imageIndex {
+                            case 0:
+                                firstThumbnail = true
+                            case 1:
+                                secondThumbnail = true
+                            case 2:
+                                thirdThumbnail = true
+                            case 3:
+                                fourthThumbnail = true
+                            case 4:
+                                fifthThumbnail = true
+                            default:
+                                print("something went wrong")
+                                
+                            }
+                            
+                        }, label: {
+                            
+                            Image("\(imageValue!)")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 100, height: 100)
+                        })
+                        .background(.orange)
+                        .cornerRadius(100.0)
+                    }.padding(.bottom, 80)
+                }.padding(.trailing, 40)
             }
-            
         }
         
     }
